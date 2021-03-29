cd7f2ba84a405cf3bd9eb20cae98f50532903bf8
Elias Bernardo <ebmm01@gmail.com>
2021-03-29 11:21:32 -0300

    Adding initial implementation of StartNode class
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

---
+++ src/uml/diagrams/activity/entities/StartNode.java
@@ -0,0 +1,15 @@
+package uml.diagrams.activity.entities;
+
+public class StartNode {
+	private String name;
+	
+	public StartNode() {}
+	
+	public StartNode(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
