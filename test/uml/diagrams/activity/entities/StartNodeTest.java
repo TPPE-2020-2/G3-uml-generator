9c3b0703123ab2448744fa5ecadcfe326a42c92f
Elias Bernardo <ebmm01@gmail.com>
2021-03-29 12:22:38 -0300

    Adding test to validate empty strings on Node name
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- test/uml/diagrams/activity/entities/StartNodeTest.java
+++ test/uml/diagrams/activity/entities/StartNodeTest.java
@@ -1,37 +1,36 @@
 package uml.diagrams.activity.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
 
 class StartNodeTest {
 	private final String NAME = "startNode";
+	private final String NAME_2 = "startNode2";
+	
 	private StartNode initialNode;
-
-	@BeforeEach
-	void setUp() throws Exception {
-		initialNode = new StartNode();
-	}
-
-	@Test
-	void testClassInstance() {
-		assertNotNull(initialNode);
-	}
 	
 	@Test
-	void testClassInstanceWithName() {
-
-		StartNode initialNode = new StartNode(NAME);
+	void testGetStartNodeName() {
+		initialNode = new StartNode(NAME);
 
 		assertEquals(NAME, initialNode.getName());
 	}
 	
 	@Test
-	void testSetNodeName() {
+	void testSetStartNodeName() {
+		initialNode = new StartNode(NAME);
 		
-		initialNode.setName(NAME);
+		initialNode.setName(NAME_2);
 		
-		assertEquals(NAME, initialNode.getName());
+		assertEquals(NAME_2, initialNode.getName());
+	}
+	
+	@Test
+	void testErrorWhenEmptyName() {
+		assertThrows(ActivityDiagramRuleException.class, () -> new StartNode(""));
 	}
 }
