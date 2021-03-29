25e55076a981264383b0b6eeaa08e1a83004a8e5
Elias Bernardo <ebmm01@gmail.com>
2021-03-29 14:56:15 -0300

    Adding FInalNode class

---
+++ src/uml/diagrams/activity/entities/FinalNode.java
@@ -0,0 +1,9 @@
+package uml.diagrams.activity.entities;
+
+import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
+
+public class FinalNode extends ActivityNode  {
+	public FinalNode(String name) throws ActivityDiagramRuleException {
+		super(name);
+	}
+}
