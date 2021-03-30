9c8e80ed4d8b342d1fbfd592724b24298fb51dbb
Elias Bernardo <ebmm01@gmail.com>
2021-03-29 23:40:29 -0300

    Adding ActivityNode implementation
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

---
+++ src/uml/diagrams/activity/entities/ActivityNode.java
@@ -0,0 +1,10 @@
+package uml.diagrams.activity.entities;
+
+import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
+
+public class ActivityNode extends BaseNode {
+	
+	public ActivityNode(String name) throws ActivityDiagramRuleException {
+		super(name);
+	}
+}
