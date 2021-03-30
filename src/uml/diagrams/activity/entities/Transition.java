219989efc99cbd14f52f8d6c2111ede6dd53aa7d
Elias Bernardo <ebmm01@gmail.com>
2021-03-30 08:43:39 -0300

    Adding getProb method with falsification
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- src/uml/diagrams/activity/entities/Transition.java
+++ src/uml/diagrams/activity/entities/Transition.java
@@ -4,7 +4,9 @@ import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
 
 public class Transition {
 	private final String INVALID_NAME_ERROR_MESSAGE = "O nome não pode ser vazio ou nulo";
+	
 	private String name;
+	private Float prob;
 	
 	public void setName(String name) throws ActivityDiagramRuleException {
 		validateNullOrEmpty(name, INVALID_NAME_ERROR_MESSAGE);
@@ -19,5 +21,13 @@ public class Transition {
 			throws ActivityDiagramRuleException {
 		if (value == null || value.trim().isEmpty())
 			throw new ActivityDiagramRuleException(errorMessage);
+	}
+
+	public Float getProb() {
+		return 1.0f;
+	}
+
+	public void setProb(Float prob) {
+		this.prob = prob;
 	}
 }
