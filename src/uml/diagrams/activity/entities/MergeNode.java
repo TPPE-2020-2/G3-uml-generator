044c39ac3d1d8a1f7b92ec739409a45eb3e75310
Elias Bernardo <ebmm01@gmail.com>
2021-03-30 00:05:33 -0300

    Adding MergeNode implementation
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

---
+++ src/uml/diagrams/activity/entities/MergeNode.java
@@ -0,0 +1,9 @@
+package uml.diagrams.activity.entities;
+
+import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
+
+public class MergeNode extends BaseNode {
+	public MergeNode(String name) throws ActivityDiagramRuleException {
+		super(name);
+	}
+}
