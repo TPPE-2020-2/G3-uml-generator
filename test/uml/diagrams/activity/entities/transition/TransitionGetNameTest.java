fa8c7800b01eb88055fba75db4cdc9f17edeb303
Elias Bernardo <ebmm01@gmail.com>
2021-03-30 08:16:30 -0300

    Adding duplicated unit test to Trasition get name
    Expecting it to fail
    
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- test/uml/diagrams/activity/entities/transition/TransitionGetNameTest.java
+++ test/uml/diagrams/activity/entities/transition/TransitionGetNameTest.java
@@ -2,26 +2,38 @@ package uml.diagrams.activity.entities.transition;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import uml.diagrams.activity.entities.Transition;
 
 class TransitionGetNameTest {
 
 	private Transition transition;
-	private final String NAME = "defaultName";
 	
 	@BeforeEach
 	public void setup() {
 		transition = new Transition();
 	}
 	
-	@Test
-	public void testGetNameOfTransition() {
-		transition.setName(NAME);
+	public static Collection transitionNames() {
+		return Arrays.asList(new Object[][] {
+	        { "defaultName" },
+	        { "transition" }
+		});
+	}
+	
+	@ParameterizedTest
+	@MethodSource("transitionNames")
+	public void testGetNameOfTransition(String name) {
+		transition.setName(name);
 		
-		assertEquals(NAME, transition.getName());
+		assertEquals(name, transition.getName());
 	}
 
 }
