4dafa83a69ea9e38b48d876f66e751fe9a912a9a
Elias Bernardo <ebmm01@gmail.com>
2021-03-30 12:02:49 -0300

    Adding unit tests to validate getActivityNode
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- test/uml/diagrams/activity/actiditydiagramelements/ActivityDiagramElementsActivityNodes.java
+++ test/uml/diagrams/activity/actiditydiagramelements/ActivityDiagramElementsActivityNodes.java
@@ -2,10 +2,12 @@ package uml.diagrams.activity.actiditydiagramelements;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,17 +19,28 @@ import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
 class ActivityDiagramElementsActivityNodes {
 
 private ActivityDiagramElements activityDiagramElements;
+	private static ActivityNode NODE_1;
+	private static ActivityNode NODE_2;
+	private static ActivityNode NODE_3;
 	
 	@BeforeEach
-	public void setup() {
+	public void setup() throws ActivityDiagramRuleException {
 		activityDiagramElements = new ActivityDiagramElements();
+	}
+	
+	@BeforeAll
+	public static void initialSetup() throws ActivityDiagramRuleException {
+		NODE_1 = new ActivityNode("node1");
+		NODE_2 = new ActivityNode("node2");
+		NODE_3 = new ActivityNode("node3");
 	}
 	
 	public static Collection<Object[]> activityNodes() throws ActivityDiagramRuleException {
 		return Arrays.asList(new Object[][] {
-	        { Arrays.asList(new ActivityNode("node1")),1 },
-	        { Arrays.asList(new ActivityNode("node1"), new ActivityNode("node2")), 2 },
-	        { Arrays.asList(new ActivityNode("node1"), new ActivityNode("node2"), new ActivityNode("node3")), 3 },
+			{ new ArrayList<ActivityNode>(), 0 },
+	        { Arrays.asList(NODE_1),1 },
+	        { Arrays.asList(NODE_1, NODE_2), 2 },
+	        { Arrays.asList(NODE_1, NODE_2, NODE_3), 3 },
 		});
 	}
 	
@@ -39,6 +52,27 @@ private ActivityDiagramElements activityDiagramElements;
 		}
 		
 		assertEquals(nodeLength, activityDiagramElements.getActivityNodes().size());
+	}
+	
+	public static Collection<Object[]> activityNodesFind() throws ActivityDiagramRuleException {
+		System.out.println(NODE_1.toString());
+		return Arrays.asList(new Object[][] {
+			{ new ArrayList<ActivityNode>(), NODE_1, null },
+			{ Arrays.asList(NODE_1), NODE_1, NODE_1},
+	        { Arrays.asList(NODE_1, NODE_2), NODE_2, NODE_2 },
+	        { Arrays.asList(NODE_1, NODE_2, NODE_3), NODE_3, NODE_3 },
+		});
+	}
+	
+	@ParameterizedTest
+	@MethodSource("activityNodesFind")
+	void testGetActivityNode(List<ActivityNode> nodes, ActivityNode nodeToFind, ActivityNode expectedNode) 
+		throws ActivityDiagramRuleException {
+		for (ActivityNode node : nodes) {
+			activityDiagramElements.addActivityNode(node);			
+		}
+		
+		assertEquals(expectedNode, activityDiagramElements.getActivityNode(nodeToFind.getName()));
 	}
 
 }
