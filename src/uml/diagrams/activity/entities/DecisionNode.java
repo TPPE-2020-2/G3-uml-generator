8f1b45f4d78809b8cb40df7044a1ccc8276b802e
Elias Bernardo <ebmm01@gmail.com>
2021-03-29 23:58:39 -0300

    Adding DecisionNode implementation
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

---
+++ src/uml/diagrams/activity/entities/DecisionNode.java
@@ -0,0 +1,9 @@
+package uml.diagrams.activity.entities;
+
+import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
+
+public class DecisionNode extends BaseNode {
+	public DecisionNode(String name) throws ActivityDiagramRuleException {
+		super(name);
+	}
+}
