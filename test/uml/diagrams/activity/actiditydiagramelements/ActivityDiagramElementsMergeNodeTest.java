d0ffdedb68a4786c7de8183831514bf50482bc17
Elias Bernardo <ebmm01@gmail.com>
2021-03-30 19:49:59 -0300

    Adding merge and validation unit tests
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

---
+++ test/uml/diagrams/activity/actiditydiagramelements/ActivityDiagramElementsMergeNodeTest.java
@@ -0,0 +1,100 @@
+package uml.diagrams.activity.actiditydiagramelements;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import uml.diagrams.activity.ActivityDiagramElements;
+import uml.diagrams.activity.entities.MergeNode;
+import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
+
+public class ActivityDiagramElementsMergeNodeTest {
+	private ActivityDiagramElements activityDiagramElements;
+
+	private static MergeNode NODE_1;
+	private static MergeNode NODE_2;
+	private static MergeNode NODE_3;
+	
+	@BeforeEach
+	public void setup() throws ActivityDiagramRuleException {
+		activityDiagramElements = new ActivityDiagramElements();
+	}
+	
+	@BeforeAll
+	public static void initialSetup() throws ActivityDiagramRuleException {
+		NODE_1 = new MergeNode("node1");
+		NODE_2 = new MergeNode("node2");
+		NODE_3 = new MergeNode("node3");
+	}
+	
+	public static Collection<Object[]> mergeNodes() throws ActivityDiagramRuleException {
+		return Arrays.asList(new Object[][] {
+			{ new ArrayList<MergeNode>(), 0 },
+	        { Arrays.asList(NODE_1),1 },
+	        { Arrays.asList(NODE_1, NODE_2), 2 },
+	        { Arrays.asList(NODE_1, NODE_2, NODE_3), 3 },
+		});
+	}
+	
+	@ParameterizedTest
+	@MethodSource("mergeNodes")
+	void testAddFinalNodes(List<MergeNode> nodes, int nodeLength) throws ActivityDiagramRuleException {
+		for (MergeNode node : nodes) {
+			activityDiagramElements.addMergeNode(node);			
+		}
+		
+		assertEquals(nodeLength, activityDiagramElements.getMergeNodes().size());
+	}
+	
+	public static Collection<Object[]> mergeNodesFind() throws ActivityDiagramRuleException {
+		return Arrays.asList(new Object[][] {
+			{ new ArrayList<MergeNode>(), NODE_1, null },
+			{ Arrays.asList(NODE_1), NODE_1, NODE_1},
+	        { Arrays.asList(NODE_1, NODE_2), NODE_2, NODE_2 },
+	        { Arrays.asList(NODE_1, NODE_2, NODE_3), NODE_3, NODE_3 },
+		});
+	}
+	
+	@ParameterizedTest
+	@MethodSource("mergeNodesFind")
+	void testGetActivityNode(List<MergeNode> nodes, MergeNode nodeToFind, MergeNode expectedNode) 
+		throws ActivityDiagramRuleException {
+		for (MergeNode node : nodes) {
+			activityDiagramElements.addMergeNode(node);			
+		}
+		
+		assertEquals(expectedNode, activityDiagramElements.getMergeNode(nodeToFind.getName()));
+	}
+	
+	public static Collection<Object[]> mergeNodesRemoveFind() throws ActivityDiagramRuleException {
+		return Arrays.asList(new Object[][] {
+			{ new ArrayList<MergeNode>(), NODE_1, new ArrayList<MergeNode>() },
+			{ Arrays.asList(NODE_1), NODE_1, new ArrayList<MergeNode>()},
+	        { Arrays.asList(NODE_1, NODE_2), NODE_2, Arrays.asList(NODE_1) },
+	        { Arrays.asList(NODE_1, NODE_2, NODE_3), NODE_3, Arrays.asList(NODE_1, NODE_2)},
+	        { Arrays.asList(NODE_2, NODE_3), NODE_1, Arrays.asList(NODE_2, NODE_3)},
+		});
+	}
+	
+	@ParameterizedTest
+	@MethodSource("mergeNodesRemoveFind")
+	void testRemoveFinalNode(List<MergeNode> nodes, MergeNode nodeToRemove, List<MergeNode> expectedList) 
+		throws ActivityDiagramRuleException {
+		for (MergeNode node : nodes) {
+			activityDiagramElements.addMergeNode(node);			
+		}
+		
+		activityDiagramElements.removeMergeNode(nodeToRemove.getName());
+		
+		assertArrayEquals(expectedList.toArray(), activityDiagramElements.getMergeNodes().toArray());
+	}
+}
---
+++ test/uml/diagrams/activity/actiditydiagramelements/ActivityDiagramElementsValidationTest.java
@@ -0,0 +1,75 @@
+package uml.diagrams.activity.actiditydiagramelements;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import uml.diagrams.activity.ActivityDiagramElements;
+import uml.diagrams.activity.entities.ActivityNode;
+import uml.diagrams.activity.entities.FinalNode;
+import uml.diagrams.activity.entities.StartNode;
+import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
+
+public class ActivityDiagramElementsValidationTest {
+	private ActivityDiagramElements activityDiagramElements;
+	private final static String NO_START_NODE_ERROR_MESSAGE = "O diagrama precisa ter um StartNode!";
+	private final static String NO_ACTIVITY_NODE_ERROR_MESSAGE = "O diagrama precisa ter pelo menos um Activity!";
+	private final static String NO_FINAL_NODE_ERROR_MESSAGE = "O diagrama precisa ter pelo menos um FinalNode!";
+	
+	@BeforeEach
+	public void setup() {
+		activityDiagramElements = new ActivityDiagramElements();
+	}
+	
+	@Test
+	public void testErrorWhenNoStartNode() throws ActivityDiagramRuleException {
+		activityDiagramElements.addActivityNode(new ActivityNode("activity"));
+		activityDiagramElements.addFinalNode(new FinalNode("final"));
+		
+		ActivityDiagramRuleException exception = assertThrows(
+				ActivityDiagramRuleException.class, 
+				() -> activityDiagramElements.validateDiagramElements());
+		
+		assertEquals(NO_START_NODE_ERROR_MESSAGE, exception.getMessage());
+	}
+	
+	@Test
+	public void testErrorWhenStartNodeIsNull() throws ActivityDiagramRuleException {
+		activityDiagramElements.setStartNode(null);
+		activityDiagramElements.addActivityNode(new ActivityNode("activity"));
+		activityDiagramElements.addFinalNode(new FinalNode("final"));
+		
+		ActivityDiagramRuleException exception = assertThrows(
+				ActivityDiagramRuleException.class, 
+				() -> activityDiagramElements.validateDiagramElements());
+		
+		assertEquals(NO_START_NODE_ERROR_MESSAGE, exception.getMessage());
+	}
+	
+	@Test
+	public void testErrorWhenNoActivityNode() throws ActivityDiagramRuleException {
+		activityDiagramElements.setStartNode(new StartNode("start"));
+		activityDiagramElements.addFinalNode(new FinalNode("final"));
+		
+		ActivityDiagramRuleException exception = assertThrows(
+				ActivityDiagramRuleException.class, 
+				() -> activityDiagramElements.validateDiagramElements());
+		
+		assertEquals(NO_ACTIVITY_NODE_ERROR_MESSAGE, exception.getMessage());
+	}
+	
+	@Test
+	public void testErrorWhenNoFInalNode() throws ActivityDiagramRuleException {
+		activityDiagramElements.setStartNode(new StartNode("start"));
+		activityDiagramElements.addActivityNode(new ActivityNode("activity"));
+		
+		ActivityDiagramRuleException exception = assertThrows(
+				ActivityDiagramRuleException.class, 
+				() -> activityDiagramElements.validateDiagramElements());
+		
+		assertEquals(NO_FINAL_NODE_ERROR_MESSAGE, exception.getMessage());
+	}
+}
