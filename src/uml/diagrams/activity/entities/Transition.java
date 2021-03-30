0d6daa1d864c6c5d6a045319e021d4e8153285a2
Elias Bernardo <ebmm01@gmail.com>
2021-03-30 08:09:06 -0300

    Fixing Transition test by faslsification method
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

---
+++ src/uml/diagrams/activity/entities/Transition.java
@@ -0,0 +1,13 @@
+package uml.diagrams.activity.entities;
+
+public class Transition {
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return "defaultName";
+	}
+}
