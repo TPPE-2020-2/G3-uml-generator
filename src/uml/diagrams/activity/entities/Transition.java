116124433665645aa260a9f50fbd1fd1ef190c79
Elias Bernardo <ebmm01@gmail.com>
2021-03-30 08:31:46 -0300

    Adding validation on null or empty name
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- src/uml/diagrams/activity/entities/Transition.java
+++ src/uml/diagrams/activity/entities/Transition.java
@@ -1,13 +1,23 @@
 package uml.diagrams.activity.entities;
 
+import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
+
 public class Transition {
+	private final String INVALID_NAME_ERROR_MESSAGE = "O nome não pode ser vazio ou nulo";
 	private String name;
 	
-	public void setName(String name) {
+	public void setName(String name) throws ActivityDiagramRuleException {
+		validateNullOrEmpty(name, INVALID_NAME_ERROR_MESSAGE);
 		this.name = name;
 	}
 	
 	public String getName() {
 		return this.name;
+	}
+	
+	private void validateNullOrEmpty(String value, String errorMessage) 
+			throws ActivityDiagramRuleException {
+		if (value == null || value.trim().isEmpty())
+			throw new ActivityDiagramRuleException(errorMessage);
 	}
 }
