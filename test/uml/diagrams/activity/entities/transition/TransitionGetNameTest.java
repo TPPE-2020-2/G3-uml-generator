a3a0ef932333b83060cdf1ec0a117244a482f9b3
Elias Bernardo <ebmm01@gmail.com>
2021-03-30 08:31:04 -0300

    Adding unit tests to validate empty or null message
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- test/uml/diagrams/activity/entities/transition/TransitionGetNameTest.java
+++ test/uml/diagrams/activity/entities/transition/TransitionGetNameTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import uml.diagrams.activity.entities.Transition;
+import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
 
 class TransitionGetNameTest {
 
@@ -31,10 +32,29 @@ class TransitionGetNameTest {
 	
 	@ParameterizedTest
 	@MethodSource("transitionNames")
-	public void testGetNameOfTransition(String name) {
+	public void testGetNameOfTransition(String name) throws ActivityDiagramRuleException {
 		transition.setName(name);
 		
 		assertEquals(name, transition.getName());
+	}
+	
+	public static Collection emptyValues() {
+		return Arrays.asList(new Object[][] {
+	        { "" },
+	        { null }
+		});
+	}
+	
+	@ParameterizedTest
+	@MethodSource("emptyValues")
+	public void testValidateEmptyOrNullName(String name) {
+		final String message = "O nome não pode ser vazio ou nulo";
+		
+		ActivityDiagramRuleException exception = assertThrows(
+				ActivityDiagramRuleException.class, 
+				() -> transition.setName(name));
+		
+		assertEquals(message, exception.getMessage());
 	}
 
 }
