0a36c4d80f9b65aa0ca6f86e28839071544be59b
Elias Bernardo <ebmm01@gmail.com>
2021-03-30 21:20:57 -0300

    Validating startNode transitions
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- src/uml/diagrams/activity/ActivityDiagramTransitions.java
+++ src/uml/diagrams/activity/ActivityDiagramTransitions.java
@@ -1,9 +1,13 @@
 package uml.diagrams.activity;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 
+import uml.diagrams.activity.entities.ActivityNode;
 import uml.diagrams.activity.entities.BaseNode;
+import uml.diagrams.activity.entities.StartNode;
 import uml.diagrams.activity.entities.Transition;
 import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
 
@@ -13,6 +17,7 @@ public class ActivityDiagramTransitions {
 	public ActivityDiagramTransitions() {
 		transitionList = new ArrayList<Transition>();
 	}
+	
 	public void addTransition(String name, float prob, BaseNode source, BaseNode target) throws ActivityDiagramRuleException {
 		Transition transition = new Transition();
 		
@@ -21,6 +26,32 @@ public class ActivityDiagramTransitions {
 		transition.setSource(source);
 		transition.setTarget(target);
 		
+		validateTransitionInsert(source, target);
+		
 		transitionList.add(transition);
+	}
+	
+	private void validateMultipleStartNodes(BaseNode source, BaseNode target) throws ActivityDiagramRuleException {
+		Transition currentStartTransition = findTransition(elem -> elem.getSource().getClass() == StartNode.class);
+		
+		if (currentStartTransition != null) 
+			throw new ActivityDiagramRuleException("Um diagrama de atividades deve ter somente um start node!");
+		
+		if (target.getClass() != ActivityNode.class)
+			throw new ActivityDiagramRuleException("Um diagrama de atividades deve se conectar somente à um activity!");
+	}
+	
+	private void validateTransitionInsert(BaseNode source, BaseNode target) throws ActivityDiagramRuleException {
+		
+		if (source.getClass() == StartNode.class)
+			validateMultipleStartNodes(source, target);
+	}
+	
+	private Transition findTransition(Predicate<Transition> filter) {
+		return transitionList
+			.stream()
+			.filter(filter)
+			.findFirst()
+			.orElse(null);
 	}
 }
