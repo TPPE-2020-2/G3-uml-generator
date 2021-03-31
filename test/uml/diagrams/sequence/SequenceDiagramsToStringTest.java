71c2897d3a0505765b09791c6b96dee04d93ce21
Lorrany Azevedo <lorrany90@gmail.com>
2021-03-31 14:34:04 -0300

    Testing to string sequence diagrams
    Co-authored-by: Lucas Maciel <lucasmacielaguiar@gmail.com>

--- test/uml/diagrams/sequence/SequenceDiagramsToStringTest.java
+++ test/uml/diagrams/sequence/SequenceDiagramsToStringTest.java
@@ -1,5 +1,32 @@
 package uml.diagrams.sequence;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import uml.diagrams.sequence.exceptions.SequenceDiagramRuleException;
+
 public class SequenceDiagramsToStringTest {
+	
+    public static Collection<Object[]>sequenceDiagramsName() {
+        return Arrays.asList(new Object[][] {
+            {"name", "<SequenceDiagrams name=\"name\" />"},
+            {"test123", "<SequenceDiagrams name=\"test123\" />"},
+            {"a", "<SequenceDiagrams name=\"a\" />"},
+            {"1", "<SequenceDiagrams name=\"1\" />"}
+        });
+    }
+    
+    @ParameterizedTest
+    @MethodSource("sequenceDiagramsName")
+    void testGetSequenceDiagramsToString(String name, String expectedString) throws SequenceDiagramRuleException {
+        SequenceDiagrams diagram = new SequenceDiagrams(name);
+
+        assertEquals(expectedString, diagram.toString());
+    }
 
 }
