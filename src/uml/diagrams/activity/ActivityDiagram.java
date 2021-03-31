826140926901df1b2ab5f9df9c98f86d3041fb5a
Elias Bernardo <ebmm01@gmail.com>
2021-03-31 14:54:42 -0300

    Finishing validations and tests on ActivityDiagram
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- src/uml/diagrams/activity/ActivityDiagram.java
+++ src/uml/diagrams/activity/ActivityDiagram.java
@@ -2,6 +2,7 @@ package uml.diagrams.activity;
 
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import uml.diagrams.activity.entities.ActivityNode;
 import uml.diagrams.activity.entities.BaseNode;
@@ -25,7 +26,7 @@ public class ActivityDiagram extends BaseNode {
 		this.activityDiagramTransitions = new ActivityDiagramTransitions();
 	}
 	
-	public <T extends BaseNode> void addNodeElement(T node) {
+	public <T extends BaseNode> ActivityDiagram addNodeElement(T node) {
 		if (node instanceof ActivityNode)
 			this.activityDiagramElements.addActivityNode((ActivityNode) node);
 		else if (node instanceof StartNode)
@@ -36,10 +37,14 @@ public class ActivityDiagram extends BaseNode {
 			this.activityDiagramElements.addMergeNode((MergeNode) node);
 		else
 			this.activityDiagramElements.addFinalNode((FinalNode) node);
+		
+		return this;
 	}
 	
-	public void addTransition(Transition transition) throws ActivityDiagramRuleException {
+	public ActivityDiagram addTransition(Transition transition) throws ActivityDiagramRuleException {
 		activityDiagramTransitions.addTransition(transition);
+		
+		return this;
 	}
 	
 	public <T extends BaseNode> void addTransition(String name, float prob, T source, T target) 
@@ -65,19 +70,94 @@ public class ActivityDiagram extends BaseNode {
 		
 		// valida se o start node possui uma conexão
 		validateStartNodeTransitionExistence(transitions);
+		validateNodesTransitions(transitions, ActivityNode.class);
+		validateNodesTransitions(transitions, DecisionNode.class);
+		validateNodesTransitions(transitions, MergeNode.class);
+		validadeFinalNodesTransitions(transitions);
 	}
 	
-	private void validateStartNodeTransitionExistence(List<Transition> transitions) throws ActivityDiagramRuleException {
-		StartNode node = activityDiagramElements.getStartNode();
+	private void validadeFinalNodesTransitions(List<Transition> transitions) throws ActivityDiagramRuleException {
+		List<Transition> finalNodeTransitions = filterTransitionsByPredicate(transitions, 
+				(elem) -> elem.getTarget().getClass() == FinalNode.class);
 		
-		if (findTransitionCount(transitions, (elem) -> elem.getSource().getClass() == StartNode.class) == 0)
+		for (FinalNode tempFinalNode: activityDiagramElements.getFinalNodes()) {
+			if (findTransitionByPredicate(finalNodeTransitions, elem -> elem.getTarget().getName() == tempFinalNode.getName()) == null)
+				throw new ActivityDiagramRuleException("O final node <> não possui uma transição para ele!".replace("<>", tempFinalNode.getName()));
+		}
+		
+	}
+
+	private void validateNodesTransitions(List<Transition> transitions, Class typeClass) throws ActivityDiagramRuleException {
+		List<Transition> sourceTransitions = filterTransitionsByPredicate(transitions, 
+					(elem) -> elem.getSource().getClass() == typeClass);
+		
+		List<Transition> targetTransitions = filterTransitionsByPredicate(transitions, 
+				(elem) -> elem.getTarget().getClass() == typeClass);
+		
+		if (typeClass == ActivityNode.class)
+			validateFowardAndBackwardNode(
+					this.activityDiagramElements.getActivityNodes(), 
+					sourceTransitions, targetTransitions, typeClass);
+		else if (typeClass == DecisionNode.class)
+			validateFowardAndBackwardNode(
+					this.activityDiagramElements.getDecisionNodes(), 
+					sourceTransitions, targetTransitions, typeClass);
+		else if (typeClass == MergeNode.class)
+			validateFowardAndBackwardNode(
+					this.activityDiagramElements.getMergeNodes(), 
+					sourceTransitions, targetTransitions, typeClass);
+	}
+	
+	private <T extends BaseNode> void validateFowardAndBackwardNode(
+			List<T> nodes, List<Transition> activitySourceTransitions, List<Transition> activityTargetTransitions, Class className) 
+				throws ActivityDiagramRuleException {
+		for (T tempNode: nodes) {
+			if (findTransitionByPredicate(activitySourceTransitions, elem -> elem.getSource().getName() == tempNode.getName()) == null)
+				throw new ActivityDiagramRuleException("<1> <2> não está ligado à nenhum elemento!"
+						.replace("<1>", className.getSimpleName())
+						.replace("<2>", tempNode.getName()));
+			
+			if (findTransitionByPredicate(activityTargetTransitions, elem -> elem.getTarget().getName() == tempNode.getName()) == null)
+				throw new ActivityDiagramRuleException("<1> <2> não é ligado por nenhum elemento pai!"
+						.replace("<1>", className.getSimpleName())
+						.replace("<2>", tempNode.getName()));
+		}
+	}
+	
+	private void validateStartNodeTransitionExistence(List<Transition> transitions) throws ActivityDiagramRuleException {	
+		if (countTransitionsByPredicate(transitions, (elem) -> elem.getSource().getClass() == StartNode.class) == 0)
 			throw new ActivityDiagramRuleException("Transição para o StartNode não encontrada!");
 	}
 	
-	private long findTransitionCount(List<Transition> transitions, Predicate<Transition> filter) {
+	private Transition findTransitionByPredicate(List<Transition> transitions, Predicate<Transition> filter) {
+		return transitions
+				.stream()
+				.filter(filter)
+				.findFirst()
+				.orElse(null);
+	}
+	
+	private List<Transition> filterTransitionsByPredicate(List<Transition> transitions, Predicate<Transition> filter) {
+		return transitions
+				.stream()
+				.filter(filter)
+				.collect(Collectors.toList());
+	}
+	
+	private long countTransitionsByPredicate(List<Transition> transitions, Predicate<Transition> filter) {
 		return transitions
 			.stream()
 			.filter(filter)
 			.count();
 	}
+	
+	@Override
+	public String toString() {
+		return "<ActivityDiagram name=\"" + name + "\">" +
+				activityDiagramElements.toString() +
+				activityDiagramTransitions.toString() +
+			"</ActivityDiagram>";
+	}
+	
+	
 }
---
+++ test/uml/diagrams/activity/ActivityDiagramToStringTest.java
@@ -0,0 +1,188 @@
+package uml.diagrams.activity;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import uml.diagrams.activity.entities.ActivityNode;
+import uml.diagrams.activity.entities.BaseNode;
+import uml.diagrams.activity.entities.DecisionNode;
+import uml.diagrams.activity.entities.FinalNode;
+import uml.diagrams.activity.entities.MergeNode;
+import uml.diagrams.activity.entities.StartNode;
+import uml.diagrams.activity.entities.Transition;
+import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
+
+public class ActivityDiagramToStringTest {
+	private ActivityDiagram activityDiagram;
+	
+	private final static String DEFAULT_NAME = "default";
+	private static StartNode startNode;
+	private static ActivityNode activityNode;
+	private static DecisionNode decisionNode;
+	private static MergeNode mergeNode;
+	private static FinalNode finalNode;
+	
+	@BeforeEach
+	public void setup() throws ActivityDiagramRuleException {
+		activityDiagram = new ActivityDiagram(DEFAULT_NAME);
+	}
+	
+	@BeforeAll
+	public static void initialSetup() throws ActivityDiagramRuleException {
+		startNode = new StartNode("start");
+		activityNode = new ActivityNode("activity");
+		decisionNode = new DecisionNode("decision");
+		mergeNode = new MergeNode("merge");
+		finalNode = new FinalNode("final");
+	}
+	
+	private <T extends BaseNode> Transition createTransition(String name, float prob, T source, T target ) 
+			throws ActivityDiagramRuleException {
+		Transition tempTransition = new Transition();
+		
+		tempTransition.setName(name);
+		tempTransition.setProb(prob);
+		tempTransition.setSource(source);
+		tempTransition.setTarget(target);
+		
+		return tempTransition;
+	}
+	
+	@Test
+	public void testSimpleToString() throws ActivityDiagramRuleException {
+		activityDiagram
+			.addNodeElement(startNode)
+			.addNodeElement(activityNode)
+			.addNodeElement(finalNode);
+		
+		Transition transition1 = createTransition("transition1", 0.5f, startNode, activityNode);
+		Transition transition2 = createTransition("transition2", 0.5f, activityNode, finalNode);
+
+		activityDiagram
+			.addTransition(transition1)
+			.addTransition(transition2);
+
+		assertDoesNotThrow(() -> activityDiagram.validateActivityDiagram());
+		
+		String expectedResult = "<ActivityDiagram name=\"default\">" +
+				"<ActivityDiagramElements>" +
+					startNode.toString() +
+					activityNode.toString() +
+					finalNode.toString() +
+				"</ActivityDiagramElements>" +
+				"<ActivityDiagramTransitions>" +
+					transition1.toString() +
+					transition2.toString() +
+				"</ActivityDiagramTransitions>" +
+				"</ActivityDiagram>";
+		
+		assertEquals(expectedResult, activityDiagram.toString());
+	}
+	
+	@Test
+	public void testSimpleToString2() throws ActivityDiagramRuleException {
+		ActivityNode activityNode2 = new ActivityNode("activity2");
+		
+		activityDiagram
+			.addNodeElement(startNode)
+			.addNodeElement(activityNode)
+			.addNodeElement(activityNode2)
+			.addNodeElement(finalNode);
+		
+		Transition transition1 = createTransition("transition1", 0.5f, startNode, activityNode);
+		Transition transition2 = createTransition("transition2", 0.5f, activityNode, activityNode2);
+		Transition transition3 = createTransition("transition2", 0.5f, activityNode2, finalNode);
+
+		activityDiagram
+			.addTransition(transition1)
+			.addTransition(transition2)
+			.addTransition(transition3);
+
+		assertDoesNotThrow(() -> activityDiagram.validateActivityDiagram());
+		
+		String expectedResult = "<ActivityDiagram name=\"default\">" +
+				"<ActivityDiagramElements>" +
+					startNode.toString() +
+					activityNode.toString() +
+					activityNode2.toString() +
+					finalNode.toString() +
+				"</ActivityDiagramElements>" +
+				"<ActivityDiagramTransitions>" +
+					transition1.toString() +
+					transition2.toString() +
+					transition3.toString() +
+				"</ActivityDiagramTransitions>" +
+				"</ActivityDiagram>";
+		
+		assertEquals(expectedResult, activityDiagram.toString());
+	}
+	
+	@Test
+	public void testToStringWithDecisionAndMergeNodes() throws ActivityDiagramRuleException {
+		ActivityNode activityNode2 = new ActivityNode("activity2");
+		ActivityNode activityNode3 = new ActivityNode("activity3");
+		ActivityNode activityNode4 = new ActivityNode("activity4");
+		
+		activityDiagram
+			.addNodeElement(startNode)
+			.addNodeElement(activityNode)
+			.addNodeElement(activityNode2)
+			.addNodeElement(activityNode3)
+			.addNodeElement(activityNode4)
+			.addNodeElement(mergeNode)
+			.addNodeElement(decisionNode)		
+			.addNodeElement(finalNode);
+		
+		Transition transition1 = createTransition("transition1", 0.5f, startNode, activityNode);
+		Transition transition2 = createTransition("transition2", 0.5f, activityNode, activityNode2);
+		Transition transition3 = createTransition("transition2", 0.5f, activityNode2, activityNode3);
+		Transition transition4 = createTransition("transition2", 0.5f, activityNode3, decisionNode);
+		Transition transition5 = createTransition("transition2", 0.5f, decisionNode, activityNode4);
+		Transition transition6 = createTransition("transition2", 0.5f, decisionNode, mergeNode);
+		Transition transition7 = createTransition("transition2", 0.5f, activityNode4, mergeNode);
+		Transition transition8 = createTransition("transition2", 0.5f, mergeNode, finalNode);
+
+		activityDiagram
+			.addTransition(transition1)
+			.addTransition(transition2)
+			.addTransition(transition3)
+			.addTransition(transition4)
+			.addTransition(transition5)
+			.addTransition(transition6)
+			.addTransition(transition7)
+			.addTransition(transition8);
+		
+		assertDoesNotThrow(() -> activityDiagram.validateActivityDiagram());
+		
+		String expectedResult = 
+			"<ActivityDiagram name=\"default\">" +
+				"<ActivityDiagramElements>" +
+					startNode.toString() +
+					activityNode.toString() +
+					activityNode2.toString() +
+					activityNode3.toString() +
+					activityNode4.toString() +
+					decisionNode.toString() +
+					mergeNode.toString() +
+					finalNode.toString() +
+				"</ActivityDiagramElements>" +
+				"<ActivityDiagramTransitions>" +
+					transition1.toString() +
+					transition2.toString() +
+					transition3.toString() +
+					transition4.toString() +
+					transition5.toString() +
+					transition6.toString() +
+					transition7.toString() +
+					transition8.toString() +
+				"</ActivityDiagramTransitions>" +
+			"</ActivityDiagram>";
+		
+		assertEquals(expectedResult, activityDiagram.toString());
+	}
+}
