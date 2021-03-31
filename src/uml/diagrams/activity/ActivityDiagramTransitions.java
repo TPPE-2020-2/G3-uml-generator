27bcc31edc6f30ba4822369cba46b66263230548
Elias Bernardo <ebmm01@gmail.com>
2021-03-30 23:04:51 -0300

    Adding more validations and unit tests to ActivityDiagramTransitions
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- src/uml/diagrams/activity/ActivityDiagramTransitions.java
+++ src/uml/diagrams/activity/ActivityDiagramTransitions.java
@@ -7,6 +7,9 @@ import java.util.function.Predicate;
 
 import uml.diagrams.activity.entities.ActivityNode;
 import uml.diagrams.activity.entities.BaseNode;
+import uml.diagrams.activity.entities.DecisionNode;
+import uml.diagrams.activity.entities.FinalNode;
+import uml.diagrams.activity.entities.MergeNode;
 import uml.diagrams.activity.entities.StartNode;
 import uml.diagrams.activity.entities.Transition;
 import uml.diagrams.activity.exceptions.ActivityDiagramRuleException;
@@ -41,12 +44,49 @@ public class ActivityDiagramTransitions {
 			throw new ActivityDiagramRuleException("Um diagrama de atividades deve se conectar somente à um activity!");
 	}
 	
-	private void validateTransitionInsert(BaseNode source, BaseNode target) throws ActivityDiagramRuleException {
+	private void validateActivityTransitions(BaseNode source, BaseNode target) throws ActivityDiagramRuleException {
+		Transition currentActivityTransition = findTransition(
+				elem -> elem.getSource().getClass() == ActivityNode.class && elem.getSource().getName() == source.getName());
 		
-		if (source.getClass() == StartNode.class)
-			validateMultipleStartNodes(source, target);
+		if (currentActivityTransition != null)
+			throw new ActivityDiagramRuleException("Não é possível ter mais de uma transição para um mesmo activity node");
 	}
 	
+	private void validateMultipleDecisionNodesTarget(BaseNode source, BaseNode target) throws ActivityDiagramRuleException {
+		Transition currentDecisionTransition = findTransition(
+				elem -> elem.getTarget().getClass() == DecisionNode.class && elem.getTarget().getName() == target.getName());
+		
+		if (currentDecisionTransition != null)
+			throw new ActivityDiagramRuleException("Não é possível um mesmo decision node ser target de múltiplos elements; Use um merge node");
+	}
+	
+	private void validateMergeTransitions(BaseNode source, BaseNode target) throws ActivityDiagramRuleException {
+		Transition currentMergeTransition = findTransition(
+				elem -> elem.getSource().getClass() == MergeNode.class && elem.getSource().getName() == source.getName());
+		
+		if (currentMergeTransition != null)
+			throw new ActivityDiagramRuleException("Não é possível um mesmo mrege node ser possuir multiplos targes; Use um decision node");
+	}
+	
+	private void validateTransitionInsert(BaseNode source, BaseNode target) throws ActivityDiagramRuleException {
+		if (source == null || target == null)
+			throw new ActivityDiagramRuleException("Não é possível inserir valores nulos!");
+		
+		if (target.getClass() == StartNode.class)
+			throw new ActivityDiagramRuleException("Um start node não pode estar numa transição como target");
+		if (target.getClass() == DecisionNode.class)
+			validateMultipleDecisionNodesTarget(source,target);
+		if (source.getClass() == StartNode.class)
+			validateMultipleStartNodes(source, target);
+		
+		if (source.getClass() == ActivityNode.class)
+			validateActivityTransitions(source, target);
+		if (source.getClass() == MergeNode.class)
+			validateMergeTransitions(source, target);
+		if (source.getClass() == FinalNode.class)
+			throw new ActivityDiagramRuleException("Um final node não pode se conectar com outro elemento como um source");
+	}
+
 	private Transition findTransition(Predicate<Transition> filter) {
 		return transitionList
 			.stream()
--- test/uml/diagrams/activity/diagramtransitions/ActivityDiagramTransitionsTest.java
+++ test/uml/diagrams/activity/diagramtransitions/ActivityDiagramTransitionsTest.java
@@ -4,11 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import uml.diagrams.activity.ActivityDiagramElements;
 import uml.diagrams.activity.ActivityDiagramTransitions;
 import uml.diagrams.activity.entities.ActivityNode;
 import uml.diagrams.activity.entities.DecisionNode;
@@ -21,7 +25,13 @@ public class ActivityDiagramTransitionsTest {
 	private ActivityDiagramTransitions diagramTransitions;
 	
 	private final static String MULTIPLE_START_NODE_ERROR_MESSAGE = "Um diagrama de atividades deve ter somente um start node!";
+	private final static String CONNECTING_WITH_START_NODE_ERROR_MESSAGE = "Um start node não pode estar numa transição como target";
 	private final static String START_NODE_WITH_NON_ACTIVITY_TARGET_ERROR_MESSAGE = "Um diagrama de atividades deve se conectar somente à um activity!";
+	private final static String NULL_VALUES_ERROR_MESSAGE = "Não é possível inserir valores nulos!";
+	private final static String MULTIPLE_ACTIVITY_TRANSITIONS_ERROR_MESSAGE = "Não é possível ter mais de uma transição para um mesmo activity node";
+	private final static String MULTIPLE_DECISION_NODE_TARGETS = "Não é possível um mesmo decision node ser target de múltiplos elements; Use um merge node";
+	private final static String MULTIPLE_MERGE_NODE_TARGETS = "Não é possível um mesmo mrege node ser possuir multiplos targes; Use um decision node";
+	private final static String FINAL_NODE_AS_SOURCE_ERROR_MESSAGE = "Um final node não pode se conectar com outro elemento como um source";
 
 	private final static String NAME = "transition";
 	private final static Float PROB = 0.5f;
@@ -73,5 +83,137 @@ public class ActivityDiagramTransitionsTest {
 		});
 		
 		assertEquals(START_NODE_WITH_NON_ACTIVITY_TARGET_ERROR_MESSAGE, exception.getMessage());
+	}
+	
+	@Test
+	public void testErrorWithNullValues() {
+		
+		ActivityDiagramRuleException exception = assertThrows(ActivityDiagramRuleException.class, () -> {
+			diagramTransitions.addTransition(NAME, PROB, null, null);
+		});
+		
+		assertEquals(NULL_VALUES_ERROR_MESSAGE, exception.getMessage());
+	}
+	
+	@Test
+	public void testActivityTransitions() {
+		
+		assertDoesNotThrow(() -> {
+			diagramTransitions.addTransition(NAME, PROB, activityNode, new ActivityNode("activity2"));
+		});
+	}
+	
+	@Test
+	public void testActivityTransitionsWithDecision() {
+		
+		assertDoesNotThrow(() -> {
+			diagramTransitions.addTransition(NAME, PROB, activityNode, decisionNode);
+		});
+	}
+	
+	@Test
+	public void testActivityTransitionsWithMerge() {
+		
+		assertDoesNotThrow(() -> {
+			diagramTransitions.addTransition(NAME, PROB, activityNode, mergeNode);
+		});
+	}
+	
+	@Test
+	public void testActivityTransitionsWithFinal() {
+		
+		assertDoesNotThrow(() -> {
+			diagramTransitions.addTransition(NAME, PROB, activityNode, finalNode);
+		});
+	}
+	
+	@Test
+	public void testErrorWithMultipleActivityTransitions() {
+		
+		ActivityDiagramRuleException exception = assertThrows(ActivityDiagramRuleException.class ,() -> {
+			diagramTransitions.addTransition(NAME, PROB, activityNode, new ActivityNode("activity2"));
+			diagramTransitions.addTransition(NAME, PROB, activityNode, new ActivityNode("activity3"));
+		});
+		
+		assertEquals(MULTIPLE_ACTIVITY_TRANSITIONS_ERROR_MESSAGE, exception.getMessage());
+	}
+	
+	@Test
+	public void testErrorWithActivityToStartNode() {
+		
+		ActivityDiagramRuleException exception = assertThrows(ActivityDiagramRuleException.class ,() -> {
+			diagramTransitions.addTransition(NAME, PROB, activityNode, startNode);
+		});
+		
+		assertEquals(CONNECTING_WITH_START_NODE_ERROR_MESSAGE, exception.getMessage());
+	}
+	
+	@Test
+	public void testDecisionNodeTransitions() {
+		
+		assertDoesNotThrow(() -> {
+			diagramTransitions.addTransition(NAME, PROB, decisionNode, activityNode);
+			diagramTransitions.addTransition(NAME, PROB, decisionNode, new ActivityNode("activity2"));
+			diagramTransitions.addTransition(NAME, PROB, decisionNode, mergeNode);
+		});
+	}
+	
+	@Test
+	public void testFailWithMultiTargetOnDecisionNode() {
+		
+		ActivityDiagramRuleException exception = assertThrows(ActivityDiagramRuleException.class, () -> {
+			diagramTransitions.addTransition(NAME, PROB, activityNode, decisionNode);
+			diagramTransitions.addTransition(NAME, PROB, new ActivityNode("activity2"), decisionNode);
+		});
+		
+		assertEquals(MULTIPLE_DECISION_NODE_TARGETS, exception.getMessage());
+	}
+	
+	@Test
+	public void testMergeNodeTransitions() {
+		
+		assertDoesNotThrow(() -> {
+			diagramTransitions.addTransition(NAME, PROB, activityNode, mergeNode);
+			diagramTransitions.addTransition(NAME, PROB, new ActivityNode("activity2"), mergeNode);
+			diagramTransitions.addTransition(NAME, PROB, decisionNode, mergeNode);
+		});
+	}
+	
+	@Test
+	public void testFailWithMultiTargetOnMergeNode() {
+		
+		ActivityDiagramRuleException exception = assertThrows(ActivityDiagramRuleException.class, () -> {
+			diagramTransitions.addTransition(NAME, PROB, mergeNode, activityNode);
+			diagramTransitions.addTransition(NAME, PROB, mergeNode, new ActivityNode("activity2"));
+		});
+		
+		assertEquals(MULTIPLE_MERGE_NODE_TARGETS, exception.getMessage());
+	}
+	
+	@Test
+	public void testFinalNodeTransitions() {
+		
+		assertDoesNotThrow(() -> {
+			diagramTransitions.addTransition(NAME, PROB, activityNode, finalNode);
+		});
+	}
+	
+	@Test
+	public void testFinalNodeMultipleTransitions() {
+		
+		assertDoesNotThrow(() -> {
+			diagramTransitions.addTransition(NAME, PROB, activityNode, finalNode);
+			diagramTransitions.addTransition(NAME, PROB, mergeNode, finalNode);
+		});
+	}
+	
+	@Test
+	public void testFailWithFinalNodesBeingSource() {
+		
+		ActivityDiagramRuleException exception = assertThrows(ActivityDiagramRuleException.class, () -> {
+			diagramTransitions.addTransition(NAME, PROB, finalNode, activityNode);
+		});
+		
+		assertEquals(FINAL_NODE_AS_SOURCE_ERROR_MESSAGE, exception.getMessage());
 	}
 }
