404df621b79694e43d6556b31cc4a5878ae05feb
Lorrany Azevedo <lorrany90@gmail.com>
2021-03-31 00:22:10 -0300

    Sequence diagram

--- src/uml/diagrams/sequence/SequenceDiagrams.java
+++ src/uml/diagrams/sequence/SequenceDiagrams.java
@@ -1,7 +1,17 @@
 package uml.diagrams.sequence;
 
-import uml.diagrams.UmlDiagram;
+import uml.diagrams.sequence.exceptions.SequenceDiagramRuleException;
 
-public class SequenceDiagrams extends UmlDiagram {
+public class SequenceDiagrams extends BaseElement {
+
+	public SequenceDiagrams(String name) throws SequenceDiagramRuleException {
+		super(name);
+		
+	}
+	
+    @Override
+    public String toString() {
+        return "<SequenceDiagrams name=\"" + this.name + "\" />";
+    }
 
 }
---
+++ test/uml/diagrams/sequence/SequenceDiagramsToStringTest.java
@@ -0,0 +1,5 @@
+package uml.diagrams.sequence;
+
+public class SequenceDiagramsToStringTest {
+
+}
