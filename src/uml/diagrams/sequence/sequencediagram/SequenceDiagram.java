0064a9afc4216267f74c7dd28e0dca64a6d97adf
lucas <lucasmacielaguiar@gmail.com>
2021-04-01 00:02:46 -0300

    Fix Sequence Diagram To String Test
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

--- src/uml/diagrams/sequence/sequencediagram/SequenceDiagram.java
+++ src/uml/diagrams/sequence/sequencediagram/SequenceDiagram.java
@@ -1,15 +1,22 @@
 package uml.diagrams.sequence.sequencediagram;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import uml.diagrams.sequence.BaseElement;
 import uml.diagrams.sequence.exceptions.SequenceDiagramRuleException;
+import uml.diagrams.sequence.lifelines.Lifeline;
+import uml.diagrams.sequence.message.Message;
 
 public class SequenceDiagram extends BaseElement {
 	final static String INVALID_GUARD_CONDITION_VALUE_ERROR_MESSAGE = "A condição de guarda não pode ser vazia ou nula";
 	private Boolean guardCondition;
 	
-
+	private List<Message> messages;
+	
 	public SequenceDiagram(String name, Boolean guardCondition) throws SequenceDiagramRuleException {
 		super(name);
+		messages = new ArrayList<>();
 
 		setGuardCondition(guardCondition);
 	}
@@ -24,12 +31,27 @@ public class SequenceDiagram extends BaseElement {
 		this.guardCondition = guardCondition;
 	}
 	
+	public void addMessage(Message message) {
+        messages.add(message);
+    }
+	
+	public List<Message> getMessages() {
+	    return messages;
+	}
+	
 	@Override
 	public String toString() {
 		String representantion = "<SequenceDiagram " + 
 				"name=\"" + this.getName() + "\" " + 
-				"guardCondition=\"" + this.getGuardCondition() + "\" " +
-				">";
+				"guardCondition=\"" + this.getGuardCondition() +
+				"\">";
+
+		for (Message message : messages) {
+            representantion += message.toString();
+        }
+		
+		representantion += "</SequenceDiagram>";
+
 		return representantion;
 	}
 }
--- test/uml/diagrams/sequence/sequencediagram/SequenceDiagramToStringTest.java
+++ test/uml/diagrams/sequence/sequencediagram/SequenceDiagramToStringTest.java
@@ -4,43 +4,73 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import uml.diagrams.sequence.exceptions.MessageFormatException;
 import uml.diagrams.sequence.exceptions.SequenceDiagramRuleException;
+import uml.diagrams.sequence.lifelines.Lifeline;
+import uml.diagrams.sequence.message.Message;
+import uml.diagrams.sequence.message.MessageAsync;
+import uml.diagrams.sequence.message.MessageSync;
+import uml.diagrams.sequence.message.Reply;
 
 public class SequenceDiagramToStringTest {
-	
-    public static Collection<Object[]>sequenceDiagramsNameTrue() {
-        return Arrays.asList(new Object[][] {
-            {"name", "<SequenceDiagram name=\"name\" guardCondition=\"true\" />"},
-            {"test123", "<SequenceDiagram name=\"test123\" guardCondition=\"true\" />"},
-            {"a", "<SequenceDiagram name=\"a\" guardCondition=\"true\" />"},
-        });
-    }
 
-    public static Collection<Object[]>sequenceDiagramsNameFalse() {
+    public static Collection<Object[]> sequenceDiagrams()
+            throws MessageFormatException, SequenceDiagramRuleException {
         return Arrays.asList(new Object[][] {
-            {"name", "<SequenceDiagram name=\"name\" guardCondition=\"false\" />"},
-            {"test123", "<SequenceDiagram name=\"test123\" guardCondition=\"false\" />"},
-            {"a", "<SequenceDiagram name=\"a\" guardCondition=\"false\" />"},
+                {
+                    "diagram1",
+                    true,
+                    Arrays.asList(
+                            new MessageSync("messagesync", 0.5f, new Lifeline("origem"), new Lifeline("destino"))
+                    ),
+                    "<SequenceDiagram name=\"diagram1\" guardCondition=\"true\">" +
+                    "<MessageSync name=\"messagesync\" prob=\"0.5\" source=\"origem\" target=\"destino\" />" +
+                    "</SequenceDiagram>"
+                },
+                {
+                    "diagram2",
+                    false,
+                    Arrays.asList(
+                            new MessageSync("messagesync", 0.5f, new Lifeline("origem"), new Lifeline("destino")),
+                            new MessageAsync("messageasync", 0.99f, new Lifeline("origem"), new Lifeline("destino"))
+                    ),
+                    "<SequenceDiagram name=\"diagram2\" guardCondition=\"false\">" +
+                    "<MessageSync name=\"messagesync\" prob=\"0.5\" source=\"origem\" target=\"destino\" />" +
+                    "<MessageAsync name=\"messageasync\" prob=\"0.99\" source=\"origem\" target=\"destino\" />" +
+                    "</SequenceDiagram>"
+                },
+                {
+                    "diagram3",
+                    true,
+                    Arrays.asList(
+                            new MessageAsync("messageasync", 0.99f, new Lifeline("origem"), new Lifeline("destino")),
+                            new MessageSync("messagesync", 0.5f, new Lifeline("origem"), new Lifeline("destino")),
+                            new Reply("reply", 0.0f, new Lifeline("origem"), new Lifeline("destino"))
+                    ),
+                    "<SequenceDiagram name=\"diagram3\" guardCondition=\"true\">" +
+                    "<MessageAsync name=\"messageasync\" prob=\"0.99\" source=\"origem\" target=\"destino\" />" +
+                    "<MessageSync name=\"messagesync\" prob=\"0.5\" source=\"origem\" target=\"destino\" />" +
+                    "<Reply name=\"reply\" prob=\"0.0\" source=\"origem\" target=\"destino\" />" +
+                    "</SequenceDiagram>"
+                }
         });
     }
 
     @ParameterizedTest
-    @MethodSource("sequenceDiagramsNameTrue")
-    void testGetSequenceDiagramsToStringTrue(String name, String expectedString) throws SequenceDiagramRuleException {
-        SequenceDiagram diagram = new SequenceDiagram(name, true);
+    @MethodSource("sequenceDiagrams")
+    void testGetSequenceDiagramsToString(String name, Boolean guard, List<Message> messages, String expectedString)
+            throws SequenceDiagramRuleException {
+        SequenceDiagram diagram = new SequenceDiagram(name, guard);
 
-        assertEquals(expectedString, diagram.toString());
-    }
-
-    @ParameterizedTest
-    @MethodSource("sequenceDiagramsNameFalse")
-    void testGetSequenceDiagramsToStringFalse(String name, String expectedString) throws SequenceDiagramRuleException {
-        SequenceDiagram diagram = new SequenceDiagram(name, false);
-
+        for (Message message : messages) {
+            diagram.addMessage(message);
+        }
+        
         assertEquals(expectedString, diagram.toString());
     }
 }
