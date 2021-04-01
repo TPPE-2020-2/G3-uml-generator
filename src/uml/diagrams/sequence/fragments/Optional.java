a6b4f998bd4279efbc0cb37dcdc1d72f828157b7
Elias Bernardo <ebmm01@gmail.com>
2021-03-31 21:19:41 -0300

    Adding implementation of Optional class
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>
    Co-authored-by: Lucas Maciel <lucasmacielaguiar@gmail.com>
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

---
+++ src/uml/diagrams/sequence/fragments/Optional.java
@@ -0,0 +1,24 @@
+package uml.diagrams.sequence.fragments;
+
+import uml.diagrams.sequence.BaseElement;
+import uml.diagrams.sequence.sequencediagram.SequenceDiagram;
+import uml.diagrams.sequence.exceptions.SequenceDiagramRuleException;
+
+public class Optional extends BaseElement {
+	private SequenceDiagram representedBy;
+	
+	public Optional(String name, SequenceDiagram representedBy) throws SequenceDiagramRuleException {
+		super(name);
+		this.representedBy = representedBy;
+	}
+	
+	public void setRepresentedBy(SequenceDiagram representedBy) {
+		this.representedBy = representedBy;
+	}
+	
+	public SequenceDiagram getRepresentedBy() {
+		return representedBy;
+	}
+	
+	// TODO toString overRide
+}
