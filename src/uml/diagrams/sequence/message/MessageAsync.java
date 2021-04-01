1e1571bfb1c7522b605ce5cddab68e85b8b077c4
lucas <lucasmacielaguiar@gmail.com>
2021-03-31 21:24:17 -0300

    Add MessageAsync To String test
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

---
+++ src/uml/diagrams/sequence/message/MessageAsync.java
@@ -0,0 +1,23 @@
+package uml.diagrams.sequence.message;
+
+import uml.diagrams.sequence.exceptions.MessageFormatException;
+import uml.diagrams.sequence.exceptions.SequenceDiagramRuleException;
+import uml.diagrams.sequence.lifelines.Lifeline;
+
+public class MessageAsync extends Message {
+
+    public MessageAsync(String name, Float prob, Lifeline source, Lifeline target)
+            throws MessageFormatException, SequenceDiagramRuleException {
+        super(name, prob, source, target);
+    }
+    
+    @Override
+    public String toString() {
+        return "<MessageAsync " + 
+                "name=\"" + this.getName() + "\" " + 
+                "prob=\"" + this.prob + "\" " + 
+                "source=\"" + this.getSource().getName() + "\" " +
+                "target=\"" + this.getTarget().getName() + "\" " +
+                "/>";
+    }
+}
