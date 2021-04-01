f48b4261688ea44c05f06132ab6017b90e6689ed
lucas <lucasmacielaguiar@gmail.com>
2021-03-31 21:28:06 -0300

    Add Reply To String test
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

---
+++ src/uml/diagrams/sequence/message/Reply.java
@@ -0,0 +1,23 @@
+package uml.diagrams.sequence.message;
+
+import uml.diagrams.sequence.exceptions.MessageFormatException;
+import uml.diagrams.sequence.exceptions.SequenceDiagramRuleException;
+import uml.diagrams.sequence.lifelines.Lifeline;
+
+public class Reply extends Message {
+
+    public Reply(String name, Float prob, Lifeline source, Lifeline target)
+            throws MessageFormatException, SequenceDiagramRuleException {
+        super(name, prob, source, target);
+    }
+    
+    @Override
+    public String toString() {
+        return "<Reply " + 
+                "name=\"" + this.getName() + "\" " + 
+                "prob=\"" + this.prob + "\" " + 
+                "source=\"" + this.getSource().getName() + "\" " +
+                "target=\"" + this.getTarget().getName() + "\" " +
+                "/>";
+    }
+}
